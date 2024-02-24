f918e3f0f80382bd1ca7ff1fb52961fad6a88913
Rizquez <pedro.rizquez.94@gmail.com>
2024-02-24 12:14:50 +0100

    Finalizacion de html y css para el diseño web

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,42 +1,25 @@
-let jsonData = null
+import jsonData from '../json/project.json' assert { type: 'json' }
 
-async function fetchData() {
-    try {
-        const response = await fetch('../../json/project.json')
-        if (!response.ok) {
-            throw new Error('Error en la conexion')
-        }
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.error('Error al cargar el JSON:', error)
-    }
-}
-
-async function processData() {
-    const data = await fetchData()
-    if (data) {
-        jsonData = data
-    }
-}
-
-document.addEventListener('DOMContentLoaded', async  (event) => {
-
-    await processData()
+// Ahora vamos a controlar los eventos del DOM realizando de manera interna la 
+// llamada a la funcion que extrae los datos del JSON.
+document.addEventListener('DOMContentLoaded', (event) => {
 
     const params = new URLSearchParams(window.location.search)
     const project = params.get('project')
 
     const projectTitle = document.querySelector('.project-title')
-    const projectImage = document.querySelector('.project-image')
+    const projectImage = document.querySelectorAll('.project-image')
     const projectDescription = document.querySelector('.project-description')
     const projectText = document.querySelector('.project-text')
+    const projectDate = document.querySelector('.project-date span')
 
     if (project) {
         projectTitle.textContent = project
         projectDescription.textContent = jsonData['projects'][project][0]
-        projectImage.src = jsonData['projects'][project][2]
+        projectImage.forEach(element => {
+            element.src = jsonData['projects'][project][2]
+        })
         projectText.textContent = jsonData['projects'][project][1]
-        
+        projectDate.innerHTML = jsonData['projects'][project][3]
     }
 })
